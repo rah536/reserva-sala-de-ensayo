2cc2282cdd5290a0bb208a1bdbc88253f39aabbe
Roberto Herrera <raherrera536@gmail.com>
2024-06-21 11:43:31 -0300

    Primer Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Reserva_SalaEnsayo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
